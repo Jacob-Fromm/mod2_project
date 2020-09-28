68a90061c05eb20249269328da204bcd67a5a5f1
jdhofmann <justindhofmann@gmail.com>
2020-09-28 11:54:05 -0400

    added user auth, login, logut

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+
+  def login
+
+  end
+  
+  def handle_login
+    @user = User.find_by(username: params[:username].downcase)
+    if @user && @user.authenticate(params[:password])
+      session[:username] = @user.id
+      redirect_to home_path
+    else
+      # byebug
+      flash[:message] = "Incorrect Username or Password"
+      redirect_to login_path
+    end
+  end
+
+  def logout
+    session[:username] = nil
+    redirect_to home_path
+  end
 end
