be391f3ba39c653f66d6c4a90d106fd1eba5c883
sdonohue0918 <sdonohue0918@gmail.com>
2020-09-30 12:27:24 -0400

    all changes as of 12:27 PM: wednesday

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,25 @@
 class ApplicationController < ActionController::Base
-#   before_action :authorized
-#   helper_method :logged_in?
+  before_action :authorized
+  helper_method :logged_in?
 
     # include NPS
     # before_action :client
     
-    # def client
-    #     @client ||= NPS::Client.new
-    # end
+    def client
+         @client ||= NPS::Client.new
+     end
 
-#   def home
-#     @current_user = User.find_by(id: session[:username])
-#   end
+  def home
+    @current_user = User.find_by(id: session[:username])
+  end
 
-#   def set_current_user
-#     @current_user = User.find_by(id: session[:username])
-#   end
+  def set_current_user
+    @current_user = User.find_by(id: session[:username])
+  end
 
-#   def logged_in?
-#     !!set_current_user
-#   end
+  def logged_in?
+    !!set_current_user
+  end
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   resources :trips
   resources :parks
+  resources :users
   get '/search', to: 'searches#new'
   post '/search', to: 'searches#show'
-  # resources :users
+   
 
 
-  # get '/login', to: 'users#login', as: 'login'
-  # post '/login', to: 'users#handle_login'
-  # delete '/logout', to: 'users#logout'
+   get '/login', to: 'users#login', as: 'login'
+   post '/login', to: 'users#handle_login'
+   delete '/logout', to: 'users#logout'
 
-  # get '/', to: 'application#home', as: 'home'
+   get '/', to: 'application#home', as: 'home'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
