c3234d99fcedfda2fcaedb5c548c3d97973b31de
jakeFromm <jacobfromm@gmail.com>
2020-09-30 15:53:36 -0400

    alerts and ammenities

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -16,6 +16,9 @@ class ParksController < ApplicationController
         if @client.parks.find {|park| park["fullName"] == @park.park_name}
             park_code = @client.parks.each.select {|park| park["fullName"] == @park.park_name}[0]["parkCode"]
             @park_api = @client.park(park_code)[0]
+            @park_amenities = @client.park_amenities(park_code)
+            @park_alerts = @client.park_alerts(park_code)
+            
         end
     
     end
