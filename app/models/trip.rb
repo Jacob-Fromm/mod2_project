0ca40714992d746625623335285ee1d3c7008073
jakeFromm <jacobfromm@gmail.com>
2020-09-30 22:26:57 -0400

    date validations on create method

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,10 @@
 class Trip < ApplicationRecord
   belongs_to :user
   belongs_to :park
+
+  validates :trip_start_date, :trip_end_date, presence: true
+  validates_date :trip_start_date, on_or_after: lambda {Date.current} 
+  validates_date :trip_end_date, after: :trip_start_date
+  
+  
 end
