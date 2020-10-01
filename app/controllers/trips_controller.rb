b12a903234f4b6707928049e107cdaf629d904bd
jdhofmann <justindhofmann@gmail.com>
2020-10-01 10:53:57 -0400

    who knows

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,25 +4,27 @@ class TripsController < ApplicationController
     end
 
     def new
-        if @current_user.valid?
-            @trip = Trip.new(user_id: @current_user)
-        else 
-            flash[:user_error] = "You are not logged in! You must be logged in to your account in order to book a trip!"
-            redirect_to login_user_path
-        end
+        
+        # if @current_user
+            @trip = Trip.new()
+        # else 
+        #     flash[:user_error] = "You are not logged in! You must be logged in to your account in order to book a trip!"
+        #     # redirect_to login_user_path
+        #     redirect_to login_path
+        # end
     end
-
+    
     def create
-        if @current_user.valid?
+        # byebug
+        # if @current_user.valid?
             @trip = @current_user.trips.create(trip_params)
             redirect_to home_path
-        end
+        # end
 
     end
 
     def edit
         @trip = Trip.find(params[:id])
-
     end
 
     def update
@@ -38,12 +40,18 @@ class TripsController < ApplicationController
             @trip.destroy
             redirect_to home_path
         end
-            
-    
     end
+
+    # def handle_date
+    #     a = '2020-10-02'
+    #     b = a.split("-").map{|n| n.to_i}
+    #     c = Date.new(b[0], b[1], b[2])
+    # end
+
+
     private
 
     def trip_params
-        params.require(:trip).permit(:user_id, :park_id)
+        params.require(:trip).permit(:user_id, :park_id, :trip_start_date, :trip_end_date)
     end
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,13 @@
 class Trip < ApplicationRecord
   belongs_to :user
   belongs_to :park
+
+  validate :start_date_before
+
+  def start_date_before
+    if self.trip_start_date.to_date > self.trip_end_date.to_date
+       puts self.trip_start_date.to_date
+      redirect_to home_path
+    end
+  end
 end
